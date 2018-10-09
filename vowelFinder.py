acc0b0cefac04326825ad8e45e5e59d1329fca75
Surya Teja <suryatejasani@gmail.com>
2018-10-09 15:59:51 +0530

    Create vowelFinder.py

---
+++ vowelFinder.py
@@ -0,0 +1,23 @@
+string = 'the fragrance of flowers spreads only in the direction of the wind aeiu'
+lis = string.split()
+print(lis)
+vowel = 0
+num = []
+for i in range(len(lis)):
+    k = lis[i]
+    for r in k:
+        if (r== 'a' or r== 'e' or r== 'i' or r== 'o' or r== 'u' ):
+            vowel = vowel +1 
+    num.append(vowel)
+    vowel = 0
+print(num)
+high = tuple((num))
+sett = max(high)
+print(sett)
+for item in range(len(lis)):
+    if num[item] == sett:
+        print(str(num[item])+' '+lis[item])
+    
+        
+
+
