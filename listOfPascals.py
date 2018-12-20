219a9666d50cde36d2917495ae65a1b87e567b66
Surya Teja <suryatejasani@gmail.com>
2018-12-20 18:07:36 +0530

    Create listOfPascals.py

---
+++ listOfPascals.py
@@ -0,0 +1,21 @@
+print("Input height of the triangle")
+heightOfPascal = int(input())
+lisPascal =[]
+for index in range(heightOfPascal-1):
+    lisPascal.append([])
+    for spaces in range(index+1):
+        lisPascal[index].append(' ')
+def appendListPascal(cur):
+    if cur == 1:
+        lisPascal[cur-1]=[1]
+    elif cur == 2:
+        lisPascal[cur-1]=[1,1]
+    else:
+        for val in range(cur):
+            if val == 0 or val == cur-1:
+                lisPascal[cur-1][val]=1
+            else:
+                lisPascal[cur-1][val]=lisPascal[cur-2][val-1]+lisPascal[cur-2][val]
+for cur in  range(1,heightOfPascal):
+    appendListPascal(cur)
+print(lisPascal) 
