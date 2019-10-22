38d8cefd5aeb09f22d1795419e0429b40ebc56c3
Surya Teja <suryatejasan@gmail.com>
2019-10-22 11:29:07 +0530

    Committing via cmd

--- Day1of100daysofcode.py
+++ Day1of100daysofcode.py
@@ -1,2 +1,8 @@
-print("Hello World")
-print("Hello GITHUB")
+#Project Day1
+import pyperclip
+print(pyperclip.paste())
+inputString = pyperclip.paste()
+listOfItems = inputString.split("\n")
+def addstar(string):
+    return "* " + string
+print("\n".join([addstar(i) for i in listOfItems]))
