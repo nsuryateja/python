82107515278ef89f9d4554b9bef03f99a2f1681d
Surya Teja <suryatejasani@gmail.com>
2019-10-22 20:00:49 +0530

    Update Day2of100daysofcode.py

--- Day2of100daysofcode.py
+++ Day2of100daysofcode.py
@@ -1,1 +1,14 @@
-
+contents = """This program counts the number of words in the given sentence
+even though it has new lines"""
+def replacer(text):
+    helper = ""
+    for i in text:
+        if i == "\n":
+            helper = helper + " "
+            continue
+        helper = helper + i
+    return helper
+def counter(text):
+    text = replacer(text)
+    return len(text.split())
+print(counter(contents))
