10adad32dc64eb6232e8a81c2b9bf96e199ebf2d
Surya Teja <suryatejasani@gmail.com>
2019-11-02 23:52:53 +0530

    Update Day44of100daysofcode.py

--- Day44of100daysofcode.py
+++ Day44of100daysofcode.py
@@ -1,1 +1,37 @@
+import json
+import difflib
+
+def data_loader():
+    """Loads the data into dictionary from a JSON file"""
+    with open("data.json") as f:
+        data = f.read()
+    return json.loads(data)
+
+def close_finder(word):
+    """Returns the close matches in the dictionary"""
+    return difflib.get_close_matches(word,data_loader(),n=1)
+
+def translator():
+    """ends if \end is inputted** word exists in dictionary, prints the defination ** If it doesnt exist,gets the close finder """
+    while True:
+        user_input = input("Enter Word:").lower()
+        if user_input == "\end":
+            break
+        if user_input in data.keys():
+            print("\n".join(data[user_input]))
+            continue
+        user_input = "".join(close_finder(user_input))
+        if user_input in data.keys():
+            confirmation = input(f"Did you mean {user_input} instead? Y or N: ").lower()
+            if confirmation == "y":
+                print("\n".join(data[user_input]))
+        else:
+            print("Word doesn't exist")
+data = data_loader()
+translator()
+
+
+
+
+
 
