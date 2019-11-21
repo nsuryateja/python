c83c0914d9528c34b54d608b223692e4d67defa5
Surya Teja <suryatejasani@gmail.com>
2019-11-21 20:21:34 +0530

    Update Day60of100daysofcode.py

--- Day60of100daysofcode.py
+++ Day60of100daysofcode.py
@@ -1,1 +1,26 @@
+def sort_ascend(list_of_non_duplicates):
+  """Implementing the sorting algorithm for non duplicates in a list - Ascending Order"""
+  iter = 0
+  while iter < len(list_of_non_duplicates):
+      index_of_min = list_of_non_duplicates.index(min(list_of_non_duplicates[iter:]))
+      popped_item = list_of_non_duplicates.pop(index_of_min)
+      list_of_non_duplicates.insert(iter,popped_item)
+      iter = iter + 1
+  return list_of_non_duplicates
+
+def sort_descend(list_of_non_duplicates):
+  """Implementing the sorting algorithm for non duplicates in a list - Descending Order"""
+  iter = 0
+  while iter < len(list_of_non_duplicates):
+      index_of_min = list_of_non_duplicates.index(min(list_of_non_duplicates[iter:]))
+      popped_item = list_of_non_duplicates.pop(index_of_min)
+      list_of_non_duplicates.insert(0,popped_item)
+      iter = iter + 1
+  return list_of_non_duplicates
+
+print(sort_ascend([1,4,3,7,5,6]))
+print(sort_descend([1,4,3,7,5,6]))
+
+
+  
 
