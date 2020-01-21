c28ed85424421de6a6c3ee92da60e6d3688a7a8a
Surya Teja <suryatejasani@gmail.com>
2020-01-22 00:46:29 +0530

    Update BalancedParenthesis.py

--- BalancedParenthesis.py
+++ BalancedParenthesis.py
@@ -1,25 +1,18 @@
-from collections import deque
-strin = '(){}{}[][}}'
+from collections import deque 
 stack = deque()
-def parrev(par):
-  if par == ")":
-    return "("
-  if par == "]":
-    return "["
-  if par == "}":
-    return "{"
-def stringCal(strin):
-  for char in strin:
-    if char in '({[':
-      stack.append(char)
-    else:
-      if not len(stack) == 0:
-        if stack[-1] == parrev(char):
-          stack.pop()
-      else:
-        return "FAIL"
-  if len(stack) == 0:
-    return "EXECUTED"
-  else:
-    return "FAIL"
-print(stringCal(strin))
+brackets = {")": "(", "]": "[",  "}": "{"}
+class Solution:
+    def isValid(self, s: str) -> bool:
+        for char in s:
+            if char in brackets:
+                stack.append(char)
+            else:
+                if not len(stack) == 0:
+                    if stack[-1] == brackets[char]:
+                        stack.pop()
+                else:
+                    return False
+        if len(stack) == 0:
+            return True
+        else:
+            return False
